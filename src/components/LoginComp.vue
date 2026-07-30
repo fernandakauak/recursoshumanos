29e3958101baabb71574d9bffe85767e45232950
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-30 17:35:19 -0400

    Login

--- src/components/LoginComp.vue
+++ src/components/LoginComp.vue
@@ -1,16 +1,83 @@
 <script setup>
     import { ref, reactive } from 'vue';
     import { http } from '../services/http.services';
+
+    const emit = defineEmits(['login-success']);
+
+    const form = reactive({
+        usuario: '',
+        password: '',
+        frasePasoMaster: '',
+    });
+
+    const showPassword = ref(false);
+    const showFrasePaso = ref(false);
+    const isLoading = ref(false);
+    const errorMessage = ref(null);
+
+    const handleLogin = async () => {
+    errorMessage.value = null;
+
+    if (!form.usuario || !form.password) {
+        errorMessage.value = 'Por favor completa el usuario y la contraseña.';
+        return;
+    }
+
+    isLoading.value = true;
+
+    try {
+    
+    const {status,data}= await http.post('/login',{
+            "username": form.usuario,
+            "password": form.password
+        });
+
+        console.log(status,data);
+        
+        if(status==200 && data?.message =='Login exitoso'){
+            guardarToken(data.token)
+            emit('login-success', true);
+        }
+            
+        } catch (err) {
+            console.log(err);
+            errorMessage.value = 'Credenciales inválidas o error de conexión.';
+        } finally {
+            isLoading.value = false;
+        }
+    };
 </script>
 
 <template>
+
     <section class="login">
-        <h1>Iniciar sesión</h1>
-        <label for="user">Usuario:</label>
-        <input type="text" name="user" id="user">
-        <label for="pass">Contraseña:</label>
-        <input type="text" name="pass" id="pass">
-        <button>Entrar</button>
+        <header>
+            <h1>Registro de Empleados</h1>
+            <h2>Departamento de Recursos Humanos - PokeMetálicos Asociados</h2>
+        </header>
+    
+        <div v-if="errorMessage" class="error">
+            {{ errorMessage }}
+        </div>
+
+        <form @submit.prevent="handleLogin">
+            <div>
+                <label>Usuario o Correo</label>
+                <input v-model="form.usuario" type="text" required placeholder="ej. Memi" />
+            </div>
+
+            <div>
+                <label>Contraseña</label>
+                <div class="password-wrapper">
+                    <input v-model="form.password" :type="showPassword ? 'text' : 'password'" required placeholder="••••••••"/>
+                    <button type="button" @click="showPassword = !showPassword" class="btn-toggle">{{ showPassword ? 'Ocultar' : 'Ver' }}</button>
+                </div>
+            </div>
+
+            <button type="submit" :disabled="isLoading" class="send">
+                {{ isLoading ? 'Iniciando sesión...' : 'Iniciar Sesión' }}
+            </button>
+        </form>
     </section>
 
 </template>
@@ -41,9 +108,20 @@
         margin-bottom: 2%;
     }
 
-    section button {
+    section button.send {
         padding: 1%;
         width: 100%;
         font-weight: bold;
+    }
+
+    .password-wrapper {
+        display: flex;
+    }
+
+    .error {
+        padding: 2%;
+        background-color: red;
+        color: white;
+        margin-bottom: 2%;
     }
 </style>
