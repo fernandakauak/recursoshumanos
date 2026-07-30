ff264f3912aaa764c4712b076cbc00d4c175d5f3
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-30 16:44:53 -0400

    Estructuras

---
+++ src/components/MainComp.vue
@@ -0,0 +1,65 @@
+<script setup></script>
+
+<template>
+    <div>
+        <header>
+            <div>
+                <h1>Registro de trabajadores</h1>
+                <p>Departamento de Recursos Humanos, PokeMetálicos Asociados</p>
+            </div>
+            <button>Agregar Empleado</button>
+        </header>
+
+    <!-- Filtros -->
+        <div>
+        <input />
+            <div>
+                <select>
+                    <option value="">Todos los cargos?</option>
+                    <option value="">OPCIÓN 1</option>
+                    <option value="">OPCIÓN 2</option>
+                    <option value="">OPCIÓN ETC</option>
+                </select>
+                <select>
+                    <option value="">Todos los departamentos</option>
+                    <option value="">OPCIÓN 1</option>
+                    <option value="">OPCIÓN 2</option>
+                    <option value="">OPCIÓN ETC</option>
+                </select>
+            </div>
+        </div>
+
+    <!-- Tabla -->
+        <div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                        <th>Nombre</th>
+                        <th>E-Mail</th>
+                        <th>Cargo</th>
+                        <th>Departamento</th>
+                        <th>Activo</th>
+                        <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Nombre y apellido</td>
+                            <td>E-Mail</td>
+                            <td>Cargo</td>
+                            <td>Departamento</td>
+                            <td>Activo</td>
+                            <td>BOTONES DE ACCIONES</td>
+                        </tr>
+                        <tr>
+                            <td>No se encontraron empleados.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
---
+++ src/components/empleadoModal.vue
@@ -0,0 +1,58 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div>
+      <div>
+        <h2>Crear o Editar Empleado</h2>
+        <button>✕</button>
+      </div>
+
+      <form>
+        <div>
+          <div>
+            <label>Nombre</label>
+            <input/>
+            <label>Apellido</label>
+            <input/>
+          </div>
+          <div>
+            <label>Cargo</label>
+            <select>
+              <option>¿OPCIONES!</option>
+            </select>
+            <label>Departamento</label>
+            <select>
+              <option>¡OPCIONES!</option>
+            </select>
+          </div>
+        </div>
+
+        <div>
+          <label>Asunto</label>
+          <input/>
+        </div>
+
+        <div>
+          <label>E-Mail</label>
+          <input type="text">
+        </div>
+
+        <div>
+          <label>Estado</label>
+          <select>
+            <option>Activo</option>
+            <option>Inactivo</option>
+          </select>
+        </div>
+
+        <div>
+          <button>Cancelar</button>
+          <button>Guardar</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style></style>
