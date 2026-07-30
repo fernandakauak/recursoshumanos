0bc7c7f407dfa2ea06eab5a8a9666268edf56bed
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-30 18:28:41 -0400

    avance modal

--- src/components/empleadoModal.vue
+++ src/components/empleadoModal.vue
@@ -1,13 +1,58 @@
-<script setup></script>
+<script setup>
+import { reactive, onMounted } from 'vue';
+  const props = defineProps({
+    ticket: { type: Object, default: null }
+  });
+
+  const emit = defineEmits(['close', 'save']);
+
+  const PRIORIDADES = ['baja', 'media', 'alta'];
+  const ESTADOS = ['abierto', 'en_proceso', 'cerrado'];
+
+  const form = reactive({
+    id:0,  
+    asunto: '',
+    descripcion: '',
+    solicitante: '',
+    prioridad: 'media',
+    estado: 'abierto',
+    solucion: '',
+    incluirDatoSeguro: false,
+    datoSeguroEtiqueta: '',
+    datoSeguroValorEnClaro: ''
+  });
+
+  onMounted(() => {
+    if (props.ticket) {
+
+      form.id = props.ticket.id;
+      form.asunto = props.ticket.asunto;
+      form.descripcion = props.ticket.descripcion;
+      form.solicitante = props.ticket.solicitante;
+      form.prioridad = props.ticket.prioridad;
+      form.estado = props.ticket.estado;
+      form.solucion = props.ticket.solucion || '';
+      if (props.ticket.datoSeguro) {
+        form.incluirDatoSeguro = true;
+        form.datoSeguroEtiqueta = props.ticket.datoSeguro.etiqueta;
+      }
+    }
+  });
+
+  const handleSubmit = () => {
+    emit('save', { ...form });
+    emit('close');
+  };
+</script>
 
 <template>
   <section>
     <header class="modal-header">
-      <h2>Crear o Editar Empleado</h2>
-      <button>✕</button>
+      <h2> {{ props.ticket ? 'Editar Ticket: ' + props.ticket.codigo : 'Crear Nuevo Ticket' }} </h2>
+      <button @click="$emit('close')">✕</button>
     </header>
 
-    <div>
+    <div  @submit.prevent="handleSubmit" class="form">
       <div class="data" >
         <label>Nombre</label>
         <input/>
@@ -43,12 +88,11 @@
           <option>Inactivo</option>
         </select>
       </div>
-
     </div>    
 
     <div class="botones">
-        <button>Cancelar</button>
-        <button>Guardar</button>
+      <button>Guardar</button>
+      <button class="cancelar">Cancelar</button>
     </div>
   </section>
   
@@ -63,7 +107,8 @@
   }
 
   .data {
-    width: 50%;
+    width: 48%;
+    padding: 1%;
   }
 
   .botones {
@@ -72,6 +117,28 @@
   }
 
   button {
-    
+    padding: 2%;
+    color: white;
+    font-weight: bold;
+    background-color: grey;
+  }
+
+  button.aceptar {
+    background-color: green;
+  }
+
+  button.cancelar {
+    background-color: red;
+  }
+
+  .form {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  select {
+    width: 100%;
+    padding: 1%;
   }
 </style>
